903b14b4b0b72c1b3fcce89e4d9074c30c698410
ansavanix <55422282+ansavanix@users.noreply.github.com>
2021-09-24 10:19:36 -0700

    Completed simon-says module. Will now solve simon-says.

--- src/main/java/com/ansavanix/ktanesolver/SimonSaysController.java
+++ src/main/java/com/ansavanix/ktanesolver/SimonSaysController.java
@@ -1,10 +1,148 @@
 package com.ansavanix.ktanesolver;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class SimonSaysController {
-    @FXML
-    private void onColorClick() {
+    private List<String> responses = new ArrayList<>();
+    private void onColorClick(String color) {
+        String response = "";
+        if (MenuController.snHasVowel) {
+            switch(MenuController.strikes){
+                case 0:
+                    switch (color) {
+                        case "red":
+                            response = "blue";
+                            break;
+                        case "blue":
+                            response = "red";
+                            break;
+                        case "green":
+                            response = "yellow";
+                            break;
+                        case "yellow":
+                            response = "green";
+                            break;
+                    }
+                    break;
+                case 1:
+                    switch (color) {
+                        case "red":
+                            response = "yellow";
+                            break;
+                        case "blue":
+                            response = "green";
+                            break;
+                        case "green":
+                            response = "blue";
+                            break;
+                        case "yellow":
+                            response = "red";
+                            break;
+                    }
+                    break;
 
+                case 2:
+                    switch (color) {
+                        case "red":
+                            response = "green";
+                            break;
+                        case "blue":
+                            response = "red";
+                            break;
+                        case "green":
+                            response = "yellow";
+                            break;
+                        case "yellow":
+                            response = "blue";
+                            break;
+                    }
+                    break;
+            }
+        }
+        else {
+            switch(MenuController.strikes){
+                case 0:
+                    switch (color) {
+                        case "red":
+                            response = "blue";
+                            break;
+                        case "blue":
+                            response = "yellow";
+                            break;
+                        case "green":
+                            response = "green";
+                            break;
+                        case "yellow":
+                            response = "red";
+                            break;
+                    }
+                    break;
+                case 1:
+                    switch (color) {
+                        case "red":
+                            response = "red";
+                            break;
+                        case "blue":
+                            response = "blue";
+                            break;
+                        case "green":
+                            response = "yellow";
+                            break;
+                        case "yellow":
+                            response = "green";
+                            break;
+                    }
+                    break;
+
+                case 2:
+                    switch (color) {
+                        case "red":
+                            response = "yellow";
+                            break;
+                        case "blue":
+                            response = "green";
+                            break;
+                        case "green":
+                            response = "blue";
+                            break;
+                        case "yellow":
+                            response = "red";
+                            break;
+                    }
+                    break;
+            }
+
+
+        }
+        responses.add(response);
+        String labelString = "";
+        for (String val : responses) {
+            labelString += val + " ";
+        }
+        output.setText(labelString);
     }
+    @FXML
+    private void onRedClick(){
+        onColorClick("red");
+    }
+    @FXML
+    private void onBlueClick() {
+        onColorClick("blue");
+    }
+    @FXML
+    private void onGreenClick() {
+        onColorClick("green");
+    }
+    @FXML
+    private void onYellowClick() {
+        onColorClick("yellow");
+    }
+    @FXML
+    Label output;
+
 }
