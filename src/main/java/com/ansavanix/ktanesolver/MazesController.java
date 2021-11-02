fbc10cde1632497e72fd48f9192efea906ac7648
ansavanix <55422282+ansavanix@users.noreply.github.com>
2021-11-01 21:50:32 -0700

    Start mazes module.

--- src/main/java/com/ansavanix/ktanesolver/MazesController.java
+++ src/main/java/com/ansavanix/ktanesolver/MazesController.java
@@ -1,4 +1,19 @@
 package com.ansavanix.ktanesolver;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class MazesController {
+
+    @FXML
+    private void onEnter() {
+
+    }
+    @FXML
+    TextField whiteLight;
+    @FXML
+    TextField redTriangle;
+    @FXML
+    Label output;
 }
