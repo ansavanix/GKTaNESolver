e2446a4f766b89ee854a35e563e0e6059ab83991
ansavanix <55422282+ansavanix@users.noreply.github.com>
2021-11-01 22:05:29 -0700

    Add TODO to MazesController.java.

--- src/main/java/com/ansavanix/ktanesolver/MazesController.java
+++ src/main/java/com/ansavanix/ktanesolver/MazesController.java
@@ -5,15 +5,12 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class MazesController {
-    //w represents a wall.
-    //p represents a point on the maze.
-    //f represents absence of a wall
+    //TODO: Find a way to store maze data in a way to make solving them easy.
     private String maze1[] = {
-            "wwwwww",
-            "pfpfpwpfpfp",
-            "fwffww",
 
-    }
+
+
+    };
     @FXML
     private void onEnter() {
 
