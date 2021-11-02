7623d55454e75527816b8f1be523e85cd9bd5416
ansavanix <55422282+ansavanix@users.noreply.github.com>
2021-11-01 22:13:09 -0700

    Elaborate on solution comment.

--- src/main/java/com/ansavanix/ktanesolver/MazesController.java
+++ src/main/java/com/ansavanix/ktanesolver/MazesController.java
@@ -7,6 +7,7 @@ import javafx.scene.control.TextField;
 public class MazesController {
     //TODO: Find a way to store maze data in a way to make solving them easy.
     //Perhaps use a 2D array for each maze and for each position store what direction the white light can go from there.
+    //From the starting position, create a branch going in every possible direction and tell the user the path of the successful branch.
     //u = can go up.
     //d = can go down.
     //r = can go right.
