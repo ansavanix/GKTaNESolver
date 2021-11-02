29ab59374bf221d0f726f32ea79e634a7bf93928
ansavanix <55422282+ansavanix@users.noreply.github.com>
2021-11-01 22:10:18 -0700

    Add possible solution comment.

--- src/main/java/com/ansavanix/ktanesolver/MazesController.java
+++ src/main/java/com/ansavanix/ktanesolver/MazesController.java
@@ -6,11 +6,13 @@ import javafx.scene.control.TextField;
 
 public class MazesController {
     //TODO: Find a way to store maze data in a way to make solving them easy.
-    private String maze1[] = {
+    //Perhaps use a 2D array for each maze and for each position store what direction the white light can go from there.
+    //u = can go up.
+    //d = can go down.
+    //r = can go right.
+    //l = can go left.
 
 
-
-    };
     @FXML
     private void onEnter() {
 
