bb23672663575709b7ee2c6fd7c99566e1e35967
ansavanix <55422282+ansavanix@users.noreply.github.com>
2021-11-14 21:36:06 -0800

    Mostly working nice GUI for mazes.

--- src/main/java/com/ansavanix/ktanesolver/MazesController.java
+++ src/main/java/com/ansavanix/ktanesolver/MazesController.java
@@ -3,6 +3,7 @@ package com.ansavanix.ktanesolver;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 
@@ -90,7 +91,8 @@ public class MazesController {
     private final String[] mazeCMarks = {"0,1 5,2", "4,1 1,3", "3,3 5,3", "0,0 0,3", "4,2 3,5", "4,0 2,4", "1,0 1,5", "3,0 2,3", "1,2 0,4"};
     //Array of all mazes
     private final String[][][] mazes = {maze1, maze2, maze3, maze4, maze5, maze6, maze7, maze8, maze9};
-
+    private final String[] toChoose = {"cmark", "whitelight", "redtriangle"};
+    int toChooseIndex = 0;
     @FXML
     TextField whiteLight;
     @FXML
@@ -99,6 +101,8 @@ public class MazesController {
     TextField cmark;
     @FXML
     Label output;
+    @FXML
+    Label instructions;
     @FXML
     ImageView zeroZero;
     @FXML
@@ -171,10 +175,39 @@ public class MazesController {
     ImageView fiveFour;
     @FXML
     ImageView fiveFive;
+    @FXML
+    ImageView cmarkImage;
+    @FXML
+    ImageView whiteLightImage;
+    @FXML
+    ImageView redTriangleImage;
+    private String cmarkCoord;
+    private String redTriangleCoord;
+    private String whiteLightCoord;
 
 
+    private String readableDirections(String directions) {
+        String readableDirections = "";
+        for (int i = 0; i < directions.length(); i++) {
+            String singularDirection = directions.substring(i,i+1);
 
-
+            switch(singularDirection) {
+                case "r":
+                    readableDirections += "Right ";
+                    break;
+                case "l":
+                    readableDirections += "Left ";
+                    break;
+                case "u":
+                    readableDirections += "Up ";
+                    break;
+                case "d":
+                    readableDirections += "Down ";
+                    break;
+            }
+        }
+        return readableDirections;
+    }
     private String opposite(String direction) {
         switch (direction) {
             case "u":
@@ -204,6 +237,7 @@ public class MazesController {
     there are multiple directions you can go in. These points of the maze are critical to know as they are points where
     you cannot go through all directions available at the same time.
      */
+    //TODO: Fix error when solving maze9. Occurs when going from coordinate 0,0 to coordinate 5,5/
     private String solveMaze(String[][] mazeArray, int sX, int sY, int eX, int eY) {
         String tempDirections = "";
         int[] checkpointX = new int[36];
@@ -264,10 +298,110 @@ public class MazesController {
 
 
         }
-        return tempDirections;
+        return readableDirections(tempDirections);
 
     }
-
+    private void coordinateClick(String coordinate) {
+        switch(toChooseIndex) {
+            case 0:
+                instructions.setText("Select the point with the white light.");
+                cmarkCoord = coordinate;
+                coordinateToImageView(coordinate).setImage(cmarkImage.getImage());
+                break;
+            case 1:
+                instructions.setText("Select the point with the red triangle.");
+                whiteLightCoord = coordinate;
+                coordinateToImageView(coordinate).setImage(whiteLightImage.getImage());
+                break;
+            default:
+                instructions.setText("Look at the directions below.");
+                redTriangleCoord = coordinate;
+                coordinateToImageView(coordinate).setImage(redTriangleImage.getImage());
+                int startX = Integer.parseInt(whiteLightCoord.substring(0, 1));
+                int startY = Integer.parseInt(whiteLightCoord.substring(2, 3));
+                int endX = Integer.parseInt(redTriangleCoord.substring(0, 1));
+                int endY = Integer.parseInt(redTriangleCoord.substring(2, 3));
+                output.setText(solveMaze(identifyMaze(cmarkCoord), startX, startY, endX, endY));
+                break;
+        }
+        toChooseIndex++;
+    }
+    private ImageView coordinateToImageView(String coordinate) {
+        switch(coordinate) {
+            case "0,0":
+                return zeroZero;
+            case "0,1":
+                return zeroOne;
+            case "0,2":
+                return zeroTwo;
+            case "0,3":
+                return zeroThree;
+            case "0,4":
+                return zeroFour;
+            case "0,5":
+                return zeroFive;
+            case "1,0":
+                return oneZero;
+            case "1,1":
+                return oneOne;
+            case "1,2":
+                return oneTwo;
+            case "1,3":
+                return oneThree;
+            case "1,4":
+                return oneFour;
+            case "1,5":
+                return oneFive;
+            case "2,0":
+                return twoZero;
+            case "2,1":
+                return twoOne;
+            case "2,2":
+                return twoTwo;
+            case "2,3":
+                return twoThree;
+            case "2,4":
+                return twoFour;
+            case "2,5":
+                return twoFive;
+            case "3,0":
+                return threeZero;
+            case "3,1":
+                return threeOne;
+            case "3,2":
+                return threeTwo;
+            case "3,3":
+                return threeThree;
+            case "3,4":
+                return threeFour;
+            case "3,5":
+                return threeFive;
+            case "4,0":
+                return fourZero;
+            case "4,1":
+                return fourOne;
+            case "4,2":
+                return fourTwo;
+            case "4,3":
+                return fourThree;
+            case "4,4":
+                return fourFour;
+            case "4,5":
+                return fourFive;
+            case "5,0":
+                return fiveZero;
+            case "5,1":
+                return fiveOne;
+            case "5,2":
+                return fiveTwo;
+            case "5,3":
+                return fiveThree;
+            case "5,4":
+                return fiveFour;
+            default:
+                return fiveFive;
+        }
+    }
 
     @FXML
     private void onEnter() {
@@ -280,4 +414,149 @@ public class MazesController {
         int endY = Integer.parseInt(endPoint.substring(2, 3));
         output.setText(solveMaze(identifyMaze(identifier), startX, startY, endX, endY));
     }
+    @FXML
+    private void clickZeroZero() {
+        coordinateClick("0,0");
+    }
+    @FXML
+    private void clickZeroOne() {
+        coordinateClick("0,1");
+    }
+    @FXML
+    private void clickZeroTwo() {
+        coordinateClick("0,2");
+    }
+    @FXML
+    private void clickZeroThree() {
+        coordinateClick("0,3");
+    }
+    @FXML
+    private void clickZeroFour() {
+        coordinateClick("0,4");
+    }
+    @FXML
+    private void clickZeroFive() {
+        coordinateClick("0,5");
+    }
+    @FXML
+    private void clickOneZero() {
+        coordinateClick("1,0");
+    }
+    @FXML
+    private void clickOneOne() {
+        coordinateClick("1,1");
+    }
+    @FXML
+    private void clickOneTwo() {
+        coordinateClick("1,2");
+    }
+    @FXML
+    private void clickOneThree() {
+        coordinateClick("1,3");
+    }
+    @FXML
+    private void clickOneFour() {
+        coordinateClick("1,4");
+    }
+    @FXML
+    private void clickOneFive() {
+        coordinateClick("1,5");
+    }
+    @FXML
+    private void clickTwoZero() {
+        coordinateClick("2,0");
+    }
+    @FXML
+    private void clickTwoOne() {
+        coordinateClick("2,1");
+    }
+    @FXML
+    private void clickTwoTwo() {
+        coordinateClick("2,2");
+    }
+    @FXML
+    private void clickTwoThree() {
+        coordinateClick("2,3");
+    }
+    @FXML
+    private void clickTwoFour() {
+        coordinateClick("2,4");
+    }
+    @FXML
+    private void clickTwoFive() {
+        coordinateClick("2,5");
+    }
+    @FXML
+    private void clickThreeZero() {
+        coordinateClick("3,0");
+    }
+    @FXML
+    private void clickThreeOne() {
+        coordinateClick("3,1");
+    }
+    @FXML
+    private void clickThreeTwo() {
+        coordinateClick("3,2");
+    }
+    @FXML
+    private void clickThreeThree() {
+        coordinateClick("3,3");
+    }
+    @FXML
+    private void clickThreeFour() {
+        coordinateClick("3,4");
+    }
+    @FXML
+    private void clickThreeFive() {
+        coordinateClick("3,5");
+    }
+    @FXML
+    private void clickFourZero() {
+        coordinateClick("4,0");
+    }
+    @FXML
+    private void clickFourOne() {
+        coordinateClick("4,1");
+    }
+    @FXML
+    private void clickFourTwo() {
+        coordinateClick("4,2");
+    }
+    @FXML
+    private void clickFourThree() {
+        coordinateClick("4,3");
+    }
+    @FXML
+    private void clickFourFour() {
+        coordinateClick("4,4");
+    }
+    @FXML
+    private void clickFourFive() {
+        coordinateClick("4,5");
+    }
+    @FXML
+    private void clickFiveZero() {
+        coordinateClick("5,0");
+    }
+    @FXML
+    private void clickFiveOne() {
+        coordinateClick("5,1");
+    }
+    @FXML
+    private void clickFiveTwo() {
+        coordinateClick("5,2");
+    }
+    @FXML
+    private void clickFiveThree() {
+        coordinateClick("5,3");
+    }
+    @FXML
+    private void clickFiveFour() {
+        coordinateClick("5,4");
+    }
+    @FXML
+    private void clickFiveFive() {
+        coordinateClick("5,5");
+    }
+
 }
